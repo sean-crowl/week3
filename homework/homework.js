2b41393680b01143f92a5f20f7c1b09bfc6448d3
Sean Crowl <sean.crowl@interapthq.com>
2016-09-22 09:43:48 -0400

    ADRIAAAAAN I DID IT

--- homework/homework.js
+++ homework/homework.js
@@ -11,8 +11,27 @@ function removeDiv(divId) {
    $("#"+divId).remove();
 }
 
-for (var i = 7; i >= 100; i++) {
-    var more = "d" + i;
+function darksky_Complete(result) {
+    var current = result.currently.summary;
+    console.log(result.currently.summary);
+    $("#current").text(current);
+
 }
+
+
+$(function() {
+
+    var request = {
+        url:
+        "https://api.darksky.net/forecast/31d5888f911f2afbeafa65f275545ee3/37.8267,-122.4233",
+        dataType: "jsonp",
+        success: darksky_Complete
+    };
+
+    $.ajax(request);
+
+    darksky_Complete();
+});
+
 
 // border-radius: 10px; height: 350px; margin: 30px; background-image:linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(http://www.disclosurenewsonline.com/wp-content/uploads/2013/02/sleet-1.jpeg); background-size: cover"><h4 style="text-align: center; color: white">Paintsville, Ky</h4><br /> <br /><p style="font-size: 62px; text-align: center; color:white">70&#8457</p><br /> <br /><h4 style="text-align: center; color: white">Sleet</h4><hr /><div class="col-md-4" style="color:white; text-align: center;">70&#8457</div><div class="col-md-4" style="color:white; text-align: center;">70 %</div><div class="col-md-4" style="color:white; text-align: center;">70&#8457</div><br /><div class="col-md-4" style="color:white; text-align: center;">min</div><div class="col-md-4" style="color:white; text-align: center;">rain</div><div class="col-md-4" style="color:white; text-align: center;">max</div>
